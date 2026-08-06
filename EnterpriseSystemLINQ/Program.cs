7c50d644409646ac02196a85d99d0c5d62de89ee
iclasTH <iclasinprogramacao@gmail.com>
2026-08-06 14:12:30 -0300

    feat: add entry point loop and a login system

--- EnterpriseSystemLINQ/Program.cs
+++ EnterpriseSystemLINQ/Program.cs
@@ -1,46 +1,88 @@
 ﻿using EnterpriseSystemLINQ.Entities;
 using System.Globalization;
 
-Console.Write("Enter full file path: ");
-string? path = Console.ReadLine();
+Console.WriteLine("Welcome to TechSolutions website!");
+Console.WriteLine("Here you can check employees informations, stored in files, if you're allowed (need to be an admin)");
+Console.WriteLine();
 
-List<Employee> employees = new List<Employee>();
+int tries = 5;
 
-try
+Console.WriteLine("Login: ");
+Console.Write("Enter your email: ");
+string? loginEmail = Console.ReadLine();
+
+Console.Write("Enter Password: ");
+string? loginPassword = Console.ReadLine();
+
+
+if (loginEmail == "admin123@gmail.com" && loginPassword == "Admin1234")
 {
-    using (StreamReader sr = File.OpenText(path))
+    Console.WriteLine("-----------------------------------------------");
+    Console.Write("Do you wanna access employees info (y/another key)? ");
+
+    char key = Console.ReadKey().KeyChar;
+    Console.WriteLine();
+
+    while (key == 'y')
     {
-        if (path == null)
+        Console.Clear();
+        Console.Write("Enter full file path: ");
+        string? path = Console.ReadLine();
+
+        List<Employee> employees = new List<Employee>();
+
+        try
         {
-            throw new ArgumentNullException();
-        }
+            using (StreamReader sr = File.OpenText(path))
+            {
+                if (path == null)
+                {
+                    throw new ArgumentNullException();
+                }
 
-        while (!sr.EndOfStream)
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(",");
+                    string name = fields[0];
+                    string email = fields[1];
+                    decimal salary = Convert.ToDecimal(fields[2], CultureInfo.InvariantCulture);
+                    employees.Add(new Employee(name, email, salary));
+                }
+
+                Console.Write("Enter salary: ");
+                decimal salaryQuery = Convert.ToDecimal(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                // Traga o email dos funcionários no qual o salário é maior do que o passado na variável
+                var getEmail = employees.Where(e => e.Salary > salaryQuery).OrderBy(e => e.Email).Select(e => e.Email);
+
+                var sum = employees.Where(e => e.Name.ToUpper()[0] == 'M').Sum(e => e.Salary);
+
+                Console.WriteLine($"Email of people whose salary is more than {salaryQuery.ToString("C2")}: ");
+                foreach (string item in getEmail)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine($"Sum of salary of people whose name starts with 'M': {sum.ToString("C2")}");
+            }
+        }
+        catch (IOException e)
         {
-            string[] fields = sr.ReadLine().Split(",");
-            string name = fields[0];
-            string email = fields[1];
-            decimal salary = Convert.ToDecimal(fields[2], CultureInfo.InvariantCulture);
-            employees.Add(new Employee(name, email, salary));
+            Console.WriteLine($"An error occurred: {e.Message}");
         }
-
-        Console.Write("Enter salary: ");
-        decimal salaryQuery = Convert.ToDecimal(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-        // Traga o email dos funcionários no qual o salário é maior do que o passado na variável
-        var getEmail = employees.Where(e => e.Salary > salaryQuery).OrderBy(e => e.Email).Select(e => e.Email);
-
-        var sum = employees.Where(e => e.Name.ToUpper()[0] == 'M').Sum(e => e.Salary);
-
-        Console.WriteLine($"Email of people whose salary is more than {salaryQuery.ToString("C2")}: ");
-        foreach (string item in getEmail)
-        {  
-            Console.WriteLine(item);   
-        }
-        Console.WriteLine($"Sum of salary of people whose name starts with 'M': {sum.ToString("C2")}");
     }
+    
 }
-catch(IOException e)
+else
 {
-    Console.WriteLine($"An error occurred: {e.Message}");
+    Console.WriteLine("Wrong email or password. ");
+    Console.WriteLine();
+    while (tries > 0)
+    {
+        Console.Write("Enter your email: ");
+        loginEmail = Console.ReadLine();
+
+        Console.Write("Enter Password: ");
+        loginPassword = Console.ReadLine();
+        tries --;
+    }
 }
